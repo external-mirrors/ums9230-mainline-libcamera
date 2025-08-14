3a60097cc70cf46e54295aad3a257adc993fc351
Otto Pflüger <otto.pflueger@abscue.de>
2025-08-14 16:31:49 +0000

    libcamera: libipa: camera_sensor: add S5K3P3 sensor properties
    Signed-off-by: Otto Pflüger <otto.pflueger@abscue.de>

--- src/ipa/libipa/camera_sensor_helper.cpp
+++ src/ipa/libipa/camera_sensor_helper.cpp
@@ -811,6 +811,17 @@ public:
 };
 REGISTER_CAMERA_SENSOR_HELPER("ov13858", CameraSensorHelperOv13858)
 
+class CameraSensorHelperS5k3p3 : public CameraSensorHelper
+{
+public:
+	CameraSensorHelperS5k3p3()
+	{
+		blackLevel_ = 4096;
+		gain_ = AnalogueGainLinear{ 1, 0, 0, 128 };
+	}
+};
+REGISTER_CAMERA_SENSOR_HELPER("s5k3p3", CameraSensorHelperS5k3p3)
+
 class CameraSensorHelperVd55g1 : public CameraSensorHelper
 {
 public:
